90171d86f829ede06c4f6166812b52b9e9ec0fce
VexxLy <alfon2312@gmail.com>
2024-01-10 17:06:14 -0400

    Commit 10/01/2024

--- src/styles.js
+++ src/styles.js
@@ -2,10 +2,13 @@ const openModal = document.querySelector('#abrir');
 const closeModal = document.querySelector('#cerrar');
 const modal = document.querySelector('#modal');
 
-openModal.addEventListener('click', () => {
+openModal.addEventListener('click', (e) => {
+    e.preventDefault();
     modal.setAttribute('open', '');
 });
 
-closeModal.addEventListener('click', () => {
+closeModal.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('llegue')
     modal.removeAttribute('open');
 });
